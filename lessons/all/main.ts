b956be4eb1bf2d0c4e46410f22b00570eb0d33c1
Buntel <julianbantel@gmail.com>
2022-08-25 14:49:58 +0200

    assigment2 add list.html

--- lessons/all/main.ts
+++ lessons/all/main.ts
@@ -1,5 +1,29 @@
-function helloWorld(): void {
-    const a = 0
-    console.log('helloWorld has been run')
+window.addEventListener('DOMContentLoaded', () => {
+    const recipe = document.querySelector('.recipe')
+    if (recipe) {
+        const shuffleButton: HTMLButtonElement = document.querySelector('.fun-zone button')!
+        shuffleButton.addEventListener('click', shuffleList)
+    } else {
+        console.log('not list.html')
+    }
+})
+
+function shuffleList(event: MouseEvent): void {
+    const list = document.querySelector('section ul')
+    if (!list) throw 'List not found'
+    const children = Array.from(list.children)
+    shuffle(children)
+    children.forEach(child => list.append(child))
 }
-helloWorld()
+/**
+ * Shuffles array in place. ES6 version
+ * @param {Array} a items An array containing the items.
+ * @see: https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+ */
+function shuffle(a: any[]) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
